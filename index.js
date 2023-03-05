b8539444bfcd15146f6f45832761a486be93f2ea
Salvador <sescamilla1999@hotmail.com>
2023-03-05 01:06:04 -0500

    testing

---
+++ index.js
@@ -0,0 +1,24 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow(){
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        frame: false, // Set to false to remove title bar
+        webPreferences: {
+          nodeIntegration: true
+        }
+      })
+      mainWindow.loadFile('index.html')
+}
+app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', function () {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+  })
+  
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+  })
